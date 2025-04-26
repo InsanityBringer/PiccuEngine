928b2cb10e821d3376b87324fbad180ecd4af2e9
InsanityBringer <1504999+InsanityBringer@users.noreply.github.com>
2025-04-26 14:54:02 -0500

    basic SDLApplication implementation

---
+++ lib/sdl/SDLApp.h
@@ -0,0 +1,61 @@
+/*
+* Descent 3: Piccu Engine
+* Copyright (C) 2024 SaladBadger
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <stdint.h>
+#include <string>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_video.h>
+
+class SDLApplication : public oeApplication
+{
+protected:
+	bool m_AppActive;
+	SDL_Window* m_window;
+	uint32_t m_flags;
+	std::string m_title;
+	void (*m_deferhandler)(bool);
+
+	SDL_Rect m_winrect = {};
+private:
+	//Runs all events in the SDL event queue. 
+	void do_events();
+	//Applies flag changes to the SDL window
+	void change_window();
+public:
+	//Initializes the SDL state 
+	SDLApplication(const char* name, unsigned flags);
+	virtual ~SDLApplication();
+	//	initializes the object
+	void init() override;
+	//	Function to retrieve information from object through a platform defined structure.
+	void get_info(void* buffer) override;
+	//	Function to get the flags
+	int flags(void) const override;
+	//	defer returns some flags.   essentially this function defers program control to OS.
+	unsigned defer() override;
+	//	suspends application for a certain amout of time...
+	void delay(float secs) override;
+	//	set a function to run when deferring to OS.
+	void set_defer_handler(void (*func)(bool)) override;
+	//  changes the flags and applies changes to the window.
+	void set_flags(int newflags) override;
+	//  moves the window
+	void set_sizepos(int x, int y, int w, int h) override;
+};
---
+++ sdl/sdlapp.cpp
@@ -0,0 +1,141 @@
+/*
+* Descent 3: Piccu Engine
+* Copyright (C) 2024 SaladBadger
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "application.h"
+#include "mono.h"
+#include "networking.h"
+#include "pserror.h"
+
+void SDLApplication::do_events()
+{
+	SDL_Event ev;
+	while (SDL_PollEvent(&ev))
+	{
+	}
+
+	if (m_deferhandler)
+		m_deferhandler(active());
+}
+
+void SDLApplication::change_window()
+{
+	if (m_flags & OEAPP_FULLSCREEN)
+	{
+		SDL_SetWindowFullscreen(m_window, true);
+	}
+	else
+	{
+		SDL_SetWindowFullscreen(m_window, false);
+		//TODO: Should allow selecting a display number
+		SDL_SetWindowPosition(m_window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+		SDL_SetWindowSize(m_window, m_winrect.w, m_winrect.h);
+	}
+	SDL_SyncWindow(m_window);
+}
+
+SDLApplication::SDLApplication(const char* name, unsigned flags) : oeApplication(), m_title(name)
+{
+	m_flags = flags;
+	m_window = nullptr;
+	m_deferhandler = nullptr;
+	m_AppActive = false;
+}
+
+SDLApplication::~SDLApplication()
+{
+	if (m_window)
+		SDL_DestroyWindow(m_window);
+
+	m_deferhandler = nullptr;
+	do_events(); //Pump events to ensure the window is closed
+}
+
+void SDLApplication::init()
+{
+	SDL_WindowFlags winflags = SDL_WINDOW_OPENGL;
+	if (m_flags & OEAPP_FULLSCREEN)
+	{
+		winflags |= SDL_WINDOW_FULLSCREEN;
+	}
+
+	m_window = SDL_CreateWindow(m_title.c_str(), 640, 480, winflags);
+	if (!m_window)
+	{
+		Error("Failed to create SDL Window! Error: %s", SDL_GetError());
+	}
+}
+
+void SDLApplication::get_info(void* buffer)
+{
+}
+
+int SDLApplication::flags(void) const
+{
+	return m_flags;
+}
+
+unsigned SDLApplication::defer()
+{
+	//I don't know if I need to do the whole "run the defer handler 6 times if the window is active" thing let's find out the hard way. 
+	do_events();
+	return 0;
+}
+
+void SDLApplication::delay(float secs)
+{
+	//this seems like it's playing with fire for large secs values
+	uint64_t ns = ((double)secs * 1000000000);
+
+	uint64_t goal = SDL_GetTicksNS() + ns;
+
+	while (SDL_GetTicksNS() < goal)
+	{
+		do_events();
+	}
+}
+
+void SDLApplication::set_defer_handler(void(*func)(bool))
+{
+	m_deferhandler = func;
+}
+
+void SDLApplication::set_flags(int newflags)
+{
+	uint32_t oldflags = m_flags;
+	m_flags = newflags;
+	if (oldflags != newflags)
+	{
+		change_window();
+	}
+}
+
+void SDLApplication::set_sizepos(int x, int y, int w, int h)
+{
+	m_winrect.x = x;
+	m_winrect.y = y;
+	m_winrect.w = w;
+	m_winrect.h = h;
+
+	if ((m_flags & OEAPP_FULLSCREEN) != 0)
+	{
+		//TODO: Should allow selecting a display number
+		SDL_SetWindowPosition(m_window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+		SDL_SetWindowSize(m_window, m_winrect.w, m_winrect.h);
+		SDL_SyncWindow(m_window);
+	}
+}
